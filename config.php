f7ad9920b0684108de4616c5ba6f15306a37f92b
Varsha Jeyarajalingam <87660633+Varsha-Jeyaraj@users.noreply.github.com>
2024-11-07 18:14:28 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'group12');
+$conn = mysqli_connect('localhost', 'csc210user', 'CSC!2qwasZX', 'group12');
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
